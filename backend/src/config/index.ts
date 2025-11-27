5075c1149466d70c2e0d442d9c4610aa5d7a0cd0
Rashedul Alam <rashedulalam112112@gmail.com>
2025-11-27 08:46:15 +0600

    chore: added shadcn components

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -5,4 +5,5 @@ export default {
   applicationPort: Number(process.env.PORT ?? "4000"),
   mongodbURI: process.env.MONGODB_URI ?? "",
   jwtSecret: process.env.JWT_SECRET ?? "",
+  openaiApiKey: process.env.OPENAI_API_KEY ?? "",
 };
